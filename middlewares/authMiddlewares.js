477a1c6972f20901c02df4a076592868d6389bf1
Ahmet Eyibil <ahmet_eyibil@hotmail.com>
2026-09-06 00:38:07 +0300

    uuid en güncel sürüm yüklendi. import yöntemi değiştirildi.

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,22 +1,27 @@
-const { v4: uuidv4 } = require('uuid');
+const uuidModule = import('uuid');
 
-const trySetQuestToken = (req, res, next) => {
+const trySetQuestToken = async (req, res, next) => {
+    try {
+        const { v4: uuidv4 } = await uuidModule;
 
-    // Eğer kullanıcının tarayıcısında guest_token çerezi yoksa oluşturalım
-    if (!req.cookies.guest_token && !req.session.user) {
-        const guestToken = uuidv4();
+        // Eğer kullanıcının tarayıcısında guest_token çerezi yoksa oluşturalım
+        if (!req.cookies.guest_token && !req.session.user) {
+            const guestToken = uuidv4();
 
-        // Çerezi 30 gün boyunca saklanacak şekilde tarayıcıya gönderiyoruz
-        res.cookie('guest_token', guestToken, {
-            maxAge: 1000 * 60 * 60 * 24 * 7, // 7 günlük token
-            httpOnly: true
-        });
-        console.log("Yeni guestToken ataması: ", guestToken);
-        req.guestToken = guestToken;
-    } else {
-        req.guestToken = req.cookies.guest_token;
+            // Çerezi 30 gün boyunca saklanacak şekilde tarayıcıya gönderiyoruz
+            res.cookie('guest_token', guestToken, {
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 günlük token
+                httpOnly: true
+            });
+            console.log("Yeni guestToken ataması: ", guestToken);
+            req.guestToken = guestToken;
+        } else {
+            req.guestToken = req.cookies.guest_token;
+        }
+        next();
+    } catch (error) {
+        next(error);
     }
-    next();
 }
 
 module.exports = {
